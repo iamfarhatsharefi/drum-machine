9fadaf1c6d30fdb555fea3f92e4820723faa8cdb
iamfarhatsharefi <sharefifarhat@gmail.com>
2024-09-13 09:34:29 -0700

    dependency solving error

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -2,7 +2,8 @@ import React, { useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 const DrumPad = ({ sound, handleDisplay }) => {
-  const playSound = () => {
+  // Memoize playSound to prevent it from changing on every render
+  const playSound = useCallback(() => {
     const audio = document.getElementById(sound.key);
     if (audio) {
       if (audio.paused) {
@@ -13,19 +14,17 @@ const DrumPad = ({ sound, handleDisplay }) => {
         handleDisplay(sound.sound);
       }
     }
-  };
+  }, [sound.key, handleDisplay]); // Memoizing based on sound.key and handleDisplay
 
-  
+  // Memoize handleKeyPress and include playSound and sound.key as dependencies
   const handleKeyPress = useCallback((event) => {
     if (event.key.toUpperCase() === sound.key) {
       playSound();
     }
   }, [sound.key, playSound]);
-  
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyPress);
-
     return () => {
       window.removeEventListener('keydown', handleKeyPress);
     };
